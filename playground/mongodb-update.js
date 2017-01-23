8c962bb5393936d3ebe66d4296ef12c48436e5ca
Dustin Podell <magicdustin14@gmail.com>
2017-01-22 18:26:39 -0800

    added update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,36 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  //findOneAndUpdate
+  db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectID('588561a6bbccbcede1706d79')
+  }, {
+    $set: {
+      completed: true
+    }
+  }, {
+    returnOriginal: false
+  }).then((res) => {
+    console.log(res);
+  });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('588564b8bbccbcede1706ea0')
+  }, {
+    $set:{name: 'Person'},
+    $inc:{age: 1}
+  }, {
+    returnOriginal: false
+  }
+  ).then((res) => {
+    console.log(res);
+  })
+
+  //db.close();
+});
