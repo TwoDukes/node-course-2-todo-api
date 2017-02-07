cdade8c4bb11454f6c1e3187c3a470b2496a5ee5
Dustin Podell <magicdustin14@gmail.com>
2017-02-07 15:46:42 -0800

    fixed small bug

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-cont port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
