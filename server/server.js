9cc2fa9c96da356e28adf2d4881664ad3ac90c7b
Dustin Podell <magicdustin14@gmail.com>
2017-02-07 15:50:18 -0800

    added test message for heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //for heroku port
 
 var app = express();
 
